1e643799eae7e4b2d51be9742f7da00a9aa4ae68
Deyvison Guilherme <deyvison.guilherme@live.com>
2017-01-18 22:52:56 -0200

    add database

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
-from flask import Flask, request, make_response
+import logging
+from flask import Flask, request
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from handlers import categorias, locais, usuarios, votacao
+from logging.handlers import RotatingFileHandler
 
-app = Flask('tcguaruja')
-app = Flask(__name__.split('.')[0])
+app = Flask(__name__)
+json = FlaskJSON(app)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def root():
-    return 'Olá mundo'
+    return json_response(status=200, mensagem='API tcguaruja')
 
+@app.errorhandler(404)
+def page_not_found():
+    return 'Page not Found'
+
+
+@app.route('/categoria/v1.0/add', methods=['POST'])
+def addcategoria():
+    negocio = categorias
+    return negocio
+
+
+@app.route('/categoria/v1.0/getlist', methods=['GET'])
+def getcategorias():
+    negocio = categorias
+    return negocio
+
+
+@app.route('/categoria/v1.0/get', methods=['GET'])
+def getcategoria():
+    negocio = categorias
+    return negocio
+
+@app.route('/categoria/v1.0/del', methods=['POST'])
+def delcategoria():
+    negocio = categorias
+    return negocio
 
 if __name__ == '__main__':
+    formatter = logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    managerror = RotatingFileHandler('logerrors.log', maxBytes=10000000, backupCount=5)
+    managerror.setLevel(logging.INFO)
+    app.logger.addHandler(managerror)
     app.run(host='127.0.0.1', port=5000, debug=True)
---
+++ handlers/categorias.py
@@ -0,0 +1,10 @@
+
+
+class Categoria(object):
+    """ Representa metodos de uma categorias"""
+    def __init__(self, arg):
+        super(Categoria, self).__init__()
+        self.arg = arg
+
+    def addcategoria():
+        pass
