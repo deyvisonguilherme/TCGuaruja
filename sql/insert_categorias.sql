99e665bd4bf506702aac2231a45b8224ab072d8d
Deyvison Guilherme <deyvison.guilherme@live.com>
2017-01-22 15:33:47 -0200

    Reset patch

--- sql/insert_categorias.sql
+++
@@ -1,8 +0,0 @@
-﻿insert into categorias(id, nm_categoria) values (nextval('categorias_id_seq'), 'Hotel');
-insert into categorias(id, nm_categoria) values (nextval('categorias_id_seq'), 'Restaurante');
-insert into categorias(id, nm_categoria) values (nextval('categorias_id_seq'), 'Praia');
-insert into categorias(id, nm_categoria) values (nextval('categorias_id_seq'), 'Trilhas');
-insert into categorias(id, nm_categoria) values (nextval('categorias_id_seq'), 'Bares');
-insert into categorias(id, nm_categoria) values (nextval('categorias_id_seq'), 'Shopping');
-insert into categorias(id, nm_categoria) values (nextval('categorias_id_seq'), 'Cinemae');
-update categorias set nm_categoria = 'Cinema' where id=8;
